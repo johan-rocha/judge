5b8d86e51c586465ced9efd5be05b3ee36045216
Isaac Batista <118384776+isaacbatista26@users.noreply.github.com>
2024-02-11 20:38:26 -0300

    Update judge.sh

--- Judge/judge.sh
+++ Judge/judge.sh
@@ -6,33 +6,25 @@ cd "$folder_submissions" || exit 1
 
 file=$(ls | grep '\.c$')
 
-# Verificar se exatamente um arquivo .c foi encontrado
 if [ "$(echo $file | wc -w)" -eq 1 ]; then
     echo "Arquivo de submissão encontrado: $file"
 
-    # Nome do executável gerado
     executable="compiled"
-    # como fazer o ./executable?, arranjar uma forma de rodar o ./
-    # Compilar o programa
     gcc "$file" -o "$executable"
-         #CMAKE utlizar flags de compilacao
-         #Compilar 2 arquivos
+    
 
-    # Verificar se a compilação foi bem-sucedida
     if [ $? -ne 0 ]; then
         echo "Compilation Error."
-        exit 1 # Saia do script com código de erro
+        exit 1
     else
         echo "Compilação bem-sucedida. Executando o programa..."
-        # Executar o programa
+        
         output_program=$(./"$executable")
 
         folder_output="../ExpectedOutput"
 
         output_expected=$(cat "$folder_output/output.txt")
 
-        # Comparar a saída com um valor esperado
-        #loop for para verificar cada caso teste de entrada
         
         if [ "$output_program" = "$output_expected" ]; then
             echo "Accepted"
@@ -44,4 +36,3 @@ if [ "$(echo $file | wc -w)" -eq 1 ]; then
 else
     echo "Nenhum arquivo .c encontrado ou mais de um arquivo encontrado na mesma pasta"
 fi
-    
